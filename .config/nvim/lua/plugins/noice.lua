ff8f689adaa37069f4bd80937231285c36984e04
Bjarne Helland <bjarne81@gmail.com>
2024-04-02 17:22:48 +0200

    update config

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -5,22 +5,5 @@ return {
     "rcarriga/nvim-notify",
   },
   event = "VeryLazy",
-  enabled = false,
-  opts = {
-    presets = { inc_rename = true },
-    views = {
-      cmdline_popup = {
-        position = {
-          row = 7,
-          col = "55%",
-        },
-      },
-      cmdline_popupmenu = {
-        position = {
-          row = 7,
-          col = "55%",
-        },
-      },
-    },
-  },
+  opts = {},
 }
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -4,11 +4,11 @@ return { -- Useful plugin to show you pending keybinds.
   config = function() -- This is the function that runs, AFTER loading
     require("which-key").setup()
     require("which-key").register({
-      -- ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-      -- ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-      -- ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-      ["<leader>f"] = { name = "[S]earch", _ = "which_key_ignore" },
-      -- ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+      ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+      ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+      ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+      ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+      ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
     })
   end,
 }
