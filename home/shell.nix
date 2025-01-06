9def6da04eeff91cc8632187f63190e605a56416
bjarnehelland <bjarne81@gmail.com>
2025-01-06 15:49:17 +0100

    zoxide

--- home/shell.nix
+++ home/shell.nix
@@ -32,6 +32,9 @@
     "...." = "cd ../../..";
     "....." = "cd ../../../..";
 
-    dive = "docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v \"$(pwd)\":\"$(pwd)\" -w \"$(pwd)\" -v \"$HOME/.dive.yaml\":\"$HOME/.dive.yaml\" ghcr.io/joschi/dive:latest";
+    take = "(){  mkdir -p $1 && cd $_; }";
+
+    dive = "docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v \"$(pwd)\":\"$(pwd)\" -w \"$(pwd)\" -v \"$HOME/.dive.yaml\":\"$HOME/.dive.yaml\" wagoodman/dive:latest";
   };
 }
+
--- scripts/setup_github_repos.sh
+++ scripts/setup_github_repos.sh
@@ -4,6 +4,9 @@
 git clone git@github.com:bjarnehelland/dotfiles.git ~/Code/bjarnehelland/dotfiles
 git clone git@github.com:bjarnehelland/100ord.git ~/Code/bjarnehelland/100ord
 
+zoxide add ~/Code/bjarnehelland/dotfiles
+zoxide add ~/Code/bjarnehelland/100ord
+
 # Stacc
 git clone git@github.com:stacc/bolig-opf.git ~/Code/stacc/bolig-opf
 git clone git@github.com:stacc/bolig-lba.git ~/Code/stacc/bolig-lba
@@ -15,3 +18,14 @@ git clone git@github.com:stacc/express-filescan.git ~/Code/stacc/express-filesca
 git clone git@github.com:stacc/environments.git ~/Code/stacc/environments
 git clone git@github.com:stacc/flow.git ~/Code/stacc/flow
 git clone git@github.com:stacc/blocc.git ~/Code/stacc/blocc
+
+zoxide add ~/Code/stacc/bolig-opf
+zoxide add ~/Code/stacc/bolig-lba
+zoxide add ~/Code/stacc/bolig-min-side
+zoxide add ~/Code/stacc/intrum
+zoxide add ~/Code/stacc/services-integration-proxy
+zoxide add ~/Code/stacc/express-sbl
+zoxide add ~/Code/stacc/express-filescan
+zoxide add ~/Code/stacc/environments
+zoxide add ~/Code/stacc/flow
+zoxide add ~/Code/stacc/blocc
