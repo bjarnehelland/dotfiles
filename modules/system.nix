9671260cb6d2017ff4afc43ec62d0e1264a162dc
bjarnehelland <bjarne81@gmail.com>
2025-01-06 20:04:08 +0100

    add hide menubar

--- modules/system.nix
+++ modules/system.nix
@@ -56,6 +56,9 @@
         Clicking = true;
         TrackpadThreeFingerDrag = true;
       };
+      NSGlobalDomain = {
+        _HIHideMenuBar = true;
+      };
     };
     keyboard = {
       enableKeyMapping = true;
